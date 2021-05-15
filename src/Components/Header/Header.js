621fcc45e98f6b0933ec6624bc91e2ac1c696b49
Bruna Romeiro <56081906+romeiro-bru@users.noreply.github.com>
2021-05-15 00:19:58 -0300

    Initial commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import atom from "./atom.svg";
+import "./style.css";
+
+export function Header() {
+  return (
+    <>
+      <h1>
+        30 days of React
+        <img src={atom} alt="react" />
+      </h1>
+      <h3>Day 8</h3>
+    </>
+  );
+}
